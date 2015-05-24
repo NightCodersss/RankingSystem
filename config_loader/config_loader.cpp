c22fad8b229920f5140e581f27d1a2ae2bb0f015
Borisenko G. and Yanush V <you@example.com>
2015-05-24 22:18:56 +0300

    compile config loader to shared library

--- config_loader/config_loader.cpp
+++ config_loader/config_loader.cpp
@@ -1,11 +1,16 @@
 #include "config_loader.hpp"
 #include <fstream>
+#include <stdexcept>
 
 ConfigLoader::ConfigLoader(std::string filename): filename(filename) { }
 
 config_type ConfigLoader::get()
 {
 	std::ifstream input(filename.c_str());
+
+	if ( !input )
+		throw std::runtime_error("Could not open file " + filename);
+
 	config_type config;
 	config << input;
 	return config;
