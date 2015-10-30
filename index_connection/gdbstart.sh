7aba5a29b371a79dcdcbf646cc492f562b5b01e6
Borisenko G. and Yanush V <you@example.com>
2015-10-30 21:30:49 +0300

    Updated flag for env for boost.log

--- index_connection/gdbstart.sh
+++ index_connection/gdbstart.sh
@@ -2,6 +2,7 @@
 
 PROJECT_PATH=/home/asio/rankingsystem
 
+export LC_ALL=C
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PROJECT_PATH/UbjsonCpp/src/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PROJECT_PATH/config_loader/
 gdb -ex run $PROJECT_PATH/index_connection/indexserver
