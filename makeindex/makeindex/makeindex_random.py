e392ebf98bfb55cea087bc30d2cf124aba0f4860
Borisenko G. and Yanush V <you@example.com>
2015-11-18 17:49:38 +0300

    Fix bug with algorithm of random index generation

--- makeindex/makeindex/makeindex_random.py
+++ makeindex/makeindex/makeindex_random.py
@@ -2,10 +2,15 @@ import subprocess
 from random import gauss, uniform
 
 def generate_lambda(c, r): # d is id of document (number)
+	"""
+		See documentation in developer documentation 
+	"""
 	l2 = sum(map(lambda x: x**2, c))
-	a = min(r, 1 - r) / (2 * l2 ** 0.5)
+	a = min(r, 1 - r) / (3 * len(c) ** 0.5 * l2 ** 0.5) # len(c)**0.5 is for diagonal of n-dimensional cube. We want it to be inside of vector c.
 	y = [uniform(-a, a) for _ in c]
-	x = map(lambda y, x: y - x + r, y, map(lambda x: x * sum(map(lambda a, b: a * b, y, c)) / l2, c))
+	x = map(lambda y, x: y - x + r, 
+			y, 
+			map(lambda x: x * sum(map(lambda a, b: a * b, y, c)) / l2, c))
 	return x
 
 def generate_file(number_of_docs, r):
@@ -15,14 +20,18 @@ def generate_file(number_of_docs, r):
 	files = [open('index.{}.index'.format(i), 'w') for i in range(n)]
 	checkfile = open('checkfile', 'w')
 
-	for d in range(number_of_docs):		
-		lambdas = generate_lambda(cs, r(d+1)) # monotonal 
-#		(lambdas, r) = generate_lambda(K)
-		map(lambda (f, l): f.write("Fairytale {} {}\n".format(d, 10000 * l)), zip(files, lambdas))
+	for d in range(number_of_docs):	# d is document id
+		lambdas = generate_lambda(cs, r(d + 1)) # monotonal 
+
+		for f, l in zip(files, lambdas):
+			f.write("Fairytale {} {}\n".format(d, l))
+
 		checkfile.write("{} {}\n".format(d, r(d+1)))
 	
 	checkfile.close()
-	map(lambda f: f.close(), files)
+
+	for f in files:
+		f.close()
 	
 	
 def read_cs(filename):
