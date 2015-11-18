3ccb3ac442ce0ed0d6f51a6b38c30af9668b0908
Borisenko G. and Yanush V <you@example.com>
2015-11-18 15:53:44 +0300

    Generate weigths for texts

---
+++ makeindex/makeindex/generate_c.py
@@ -0,0 +1,25 @@
+import random
+import matplotlib.pyplot as plt
+
+def generate(n, s):
+	numbers = [random.random() for _ in xrange(n)]
+	numbers += [0, s]
+	numbers.sort()
+
+	result = []
+	for i in xrange(1, len(numbers)):
+		result.append(numbers[i] - numbers[i - 1])
+
+	result.sort()
+
+#	plt.imshow(result, aspect='auto')
+#	plt.show()
+
+#	plt.hist(result, bins=100)
+	
+#	plt.plot(result)
+#	plt.show()
+
+	return result
+
+print ' '.join(map(str, generate(100, 1.0)))
