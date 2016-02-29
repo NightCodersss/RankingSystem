d46a0b8ce816eb7996ab28ad87d71976e01cc8b6
Borisenko G. and Yanush V <you@example.com>
2016-02-29 18:48:30 +0300

    Added script to rename strings by replace rules in files
    For example Index -> ForwardIndex

---
+++ rename.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cat $1 | while read line
+do
+	IFS=" " read -r -a replacement <<< "$line";
+	echo $line
+	from=(${replacement[0]})
+	to=(${replacement[1]})
+
+	echo "Replace $from to $to"
+
+	for filename in `find -name \*.cpp` `find -name \*.hpp`
+	do
+		sed -i -e "s/$from/$to/g" $filename
+	done
+	
+	for name in `ls $from*` 
+	do
+	mv "$name" "${name/$from/$to}"
+	done
+done
