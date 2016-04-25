b287996bbaeb6fadd2c60d5fb812c602606649a6
Georgy Borisenko <gosha-bor@yandex.ru>
2016-04-25 19:39:11 +0300

    Splitting doc into texts (first sentence and other)

--- indexer/mine_texts.py
+++ indexer/mine_texts.py
@@ -6,8 +6,23 @@ import sys
 if len(sys.argv) < 2:
 	raise StandartError("usage: script.py <doc to parse>")
 
-# TODO: make text separation
 doc_name = sys.argv[1]
-with open(doc_name) as doc, open("full.text", "w") as text:
-	text.write(doc.read())
 
+texts = set([
+	#"full", 
+	"first_sentence", 
+	"body"
+	])
+
+if "full" in texts:
+	with open(doc_name) as doc, open("full.text", "w") as text:
+		text.write(doc.read())
+elif "first_sentence" in texts:
+	with open(doc_name) as doc, \
+		open("first_sentence.text", "w") as first_sentence_file, \
+		open("body.text", "w") as body_file:
+
+		sentences = doc.read().split('.')
+
+		first_sentence_file.write(sentences[0])
+		body_file.write('.'.join(sentences[1:-1]))
