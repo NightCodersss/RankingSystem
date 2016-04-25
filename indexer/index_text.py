42787e10bd236039ca9b4a8293b1cd89b0657eff
Georgy Borisenko <gosha-bor@yandex.ru>
2016-04-25 19:14:10 +0300

    Stemming, removing stopwords and one-letter words

--- indexer/index_text.py
+++ indexer/index_text.py
@@ -1,6 +1,8 @@
 import sys
 import nltk
 import nltk.probability
+import nltk.stem
+import nltk.corpus
 
 if len(sys.argv) < 4:
 	raise StandartError("usage: script.py <text_file> <doc_id> <commit_file>")
@@ -11,7 +13,12 @@ commit = open(sys.argv[3], "a")
 
 text = text_file.read().decode('utf-8') 
 
-fdict = nltk.probability.FreqDist(token.lower().encode('utf-8') for token in nltk.word_tokenize(text))
+stemmer = nltk.stem.SnowballStemmer("russian")
+
+fdict = nltk.probability.FreqDist(stemmer.stem(token.lower()).encode('utf-8') 
+	for token in nltk.word_tokenize(text) 
+		if not token in nltk.corpus.stopwords.words("russian") 
+			and len(token) >= 2)
 for word in fdict:
 	print "Word to commit: ", word
 	commit.write("{word} {doc_id} {freq}\n".format(
