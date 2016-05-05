cc564b92d0d3e30b329ba0b5be9b37285cb8f94f
Borisenko G. and Yanush V <you@example.com>
2016-05-05 12:22:03 +0300

    Stem by pymorphy2

--- indexer/stem_provider.py
+++ indexer/stem_provider.py
@@ -1,7 +1,23 @@
-import nltk
-import nltk.stem
+stem_policity = "pymorphy" # nltk, pymorphy
 
-stemmer = nltk.stem.SnowballStemmer("russian")
+if stem_policity == "nltk":
+	import nltk
+	import nltk.stem
+	stemmer = nltk.stem.SnowballStemmer("russian")
+
+elif stem_policity == "pymorphy":
+	import nltk.probability
+	import pymorphy2
+	morph = pymorphy2.MorphAnalyzer()
+
+else:
+	import sys
+	print "unknown stem_policity: ", stem_policity
+	sys.exit(1)
 
 def stem(word):
-	return stemmer.stem(word.lower())
+	if stem_policity == "nltk":
+		return stemmer.stem(word.lower())
+	elif stem_policity == "pymorphy":
+		common = nltk.probability.FreqDist(form.normal_form for form in morph.parse(word.lower())).most_common(1)
+		return (common[0][0])
